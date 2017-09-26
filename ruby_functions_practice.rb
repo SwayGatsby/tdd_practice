540c1a41ae4e9fc8e69c4f87e2a51161a1b14403
SwayGatsby <kayla.fowler@hotmail.com>
2017-09-26 16:45:46 +0100

    9 tests passed

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,34 +1,50 @@
-def return_10()
-  return 10
-end
+# def return_10()
+#   return 10
+# end
+#
+#
+# def add(num_1, num_2)
+#   return num_1 + num_2
+# end
+#
+#
+# def subtract (num_1, num_2)
+#   return num_1 - num_2
+# end
+#
+#
+# def multiply (num_1, num_2)
+#   return num_1 * num_2
+# end
+#
+#
+# def divide (num_1, num_2)
+#   return num_1 / num_2
+# end
+#
+#
+# def length_of_string(string)
+#   return string.length
+# end
+#
+#
+# def join_string(string_1, string_2)
+#   return string_1 + string_2
+# end
+#
+#
+# def add_string_as_number(string_1, string_2)
+#   return string_1.to_i + string_2.to_i
+# end
 
 
-def add(num_1, num_2)
-  return num_1 + num_2
-end
-
-
-def subtract (num_1, num_2)
-  return num_1 - num_2
-end
-
-def multiply (num_1, num_2)
-  return num_1 * num_2
-end
-
-
-def divide (num_1, num_2)
-  return num_1 / num_2
-end
-
-def length_of_string(string)
-  return string.length
-end
-
-def join_string(string_1, string_2)
-  return string_1 + string_2
-end
-
-def add_string_as_number(string_1, string_2)
-  return string_1.to_i + string_2.to_i
-end
+# def number_to_full_month_name(month_string)
+#   return case month_string
+#   when 1
+#     "January"
+#   when 3
+#     "March"
+#   when 9
+#     "September"
+#   end
+# end
